7525e1d8defdf45be3b0d1186fb7eeeb21f0e298
Ojasvi Yadav <ojasviyadav2308@gmail.com>
2026-07-23 14:14:13 +0530

    java script

---
+++ app.js
@@ -0,0 +1,16 @@
+// functions in javascript
+/***console.log("basic javascript function known as synchronous function");
+function hello(){
+    console.log("this js function");
+}
+hello();
+console.log("synchronous javascript");***/
+
+// function as parameter argument  
+function hello(n1,n2){
+console.log (n1+n2);
+console.log(arguments);
+}
+let a=10;
+let b=9;
+hello(a,b);
